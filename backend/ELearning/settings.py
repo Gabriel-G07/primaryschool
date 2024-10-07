aed686afba23729ec323217c7fa371d74f2a0940
Gabriel G <07gabrielg07@gmail.com>
2024-10-07 13:33:31 +0200

    Templates position change

--- backend/ELearning/settings.py
+++ backend/ELearning/settings.py
@@ -128,7 +128,7 @@ ROOT_URLCONF = 'ELearning.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / '../templates'],
+        'DIRS': [BASE_DIR / './templates'],
         
         'APP_DIRS': True,
         'OPTIONS': {
